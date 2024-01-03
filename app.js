5feb648aa3a8687646e673843542b22730712328
shreejan shrestha <shr.shreejan@gmail.com>
2024-01-03 20:39:45 +0545

    Added multiple user login support using json file

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-require('dotenv').config();
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 
-const DP = process.env.DP;
-const USERNAME = process.env.USERNAME;
-const PASSWORD = process.env.PASSWORD;
-
-(async () => {
-  const browser = await puppeteer.launch();
+const login = async (user) => {
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36');
   await page.setViewport({ width: 1920, height: 1080 });
 
-  await page.goto('https://meroshare.cdsc.com.np/#/login');
+  let loginPage = await page.goto('https://meroshare.cdsc.com.np/#/login');
+  if(loginPage.status() != 404){
+    console.log("status: ok");
+  }
   await page.waitForSelector('.sign-in');
 
   const selectDp = await page.$('.select2-selection__placeholder');
   if (selectDp) {
     await selectDp.click();
-    await selectDp.type(DP);
+    await selectDp.type(user.dp);
     await selectDp.press('Enter');
   }
 
   const username = await page.$('#username');
   if(username) {
     await username.click();
-    await username.type(USERNAME);
+    await username.type(user.username);
   }
 
   const password = await page.$('#password');
   if(password) {
     await password.click();
-    await password.type(PASSWORD);
+    await password.type(user.password);
   }
 
   const signIn = await page.$('.btn.sign-in');
@@ -38,7 +38,13 @@ const PASSWORD = process.env.PASSWORD;
   }
 
   await page.waitForSelector('.user-profile-role');
-  await page.screenshot({ path: 'debug.png' });
+  await page.screenshot({ path: `${user.username}_debug.png` });
 
-  await browser.close();
-})();
+  browser.close();
+};
+
+const users = JSON.parse(fs.readFileSync('sample.json')).users;
+
+for (const user of users) {
+  await login(user);
+}
