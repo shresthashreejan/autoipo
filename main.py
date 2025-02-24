a23440cc349f369a54facf4d075862e6f98c3dba
shreejan shrestha <shr.shreejan@gmail.com>
2025-02-24 11:05:02 +0545

    Added changes for better error handling

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def main():
             for user in users:
                 browser = p.chromium.launch(headless=False, slow_mo=100)
                 page = browser.new_page()
+                username = user['username']
+                name = user['name']
 
                 try:
                     page.goto('https://meroshare.cdsc.com.np/#/login')
@@ -42,33 +44,40 @@ def main():
                         if share_of_type_element:
                             share_of_type_text = share_of_type_element.inner_text().strip()
                             if share_of_type_text in ['IPO', 'FPO']:
-                                apply_button = company_list.query_selector('xpath=//div[contains(@class, "action-buttons")]//button[contains(@class, "btn-issue") and .//i[contains(text(), "Apply")]]')
-                                if apply_button:
-                                    apply_button.click()
+                                isin_element = company_list.query_selector('.isin')
+                                if isin_element and isin_element.get_attribute('tooltip') == 'Share Group' and isin_element.inner_text().strip() == 'Ordinary Shares':
+                                    apply_button = company_list.query_selector('xpath=//div[contains(@class, "action-buttons")]//button[contains(@class, "btn-issue") and .//i[contains(text(), "Apply")]]')
+                                    if apply_button:
+                                        apply_button.click()
 
-                                    # Fill application form
-                                    form_group = page.query_selector('.form-group:has-text("Minimum Quantity")')
-                                    minimum_quantity_value = 0
-                                    minimum_quantity_element = form_group.query_selector('.form-value span')
-                                    if minimum_quantity_element:
-                                        minimum_quantity_value = minimum_quantity_element.inner_text().strip()
+                                        # Fill application form
+                                        form_group = page.query_selector('.form-group:has-text("Minimum Quantity")')
+                                        minimum_quantity_value = 0
+                                        minimum_quantity_element = form_group.query_selector('.form-value span')
+                                        if minimum_quantity_element:
+                                            minimum_quantity_value = minimum_quantity_element.inner_text().strip()
 
-                                    page.select_option('#selectBank', index=1)
-                                    page.select_option('#accountNumber', index=1)
-                                    page.locator('#appliedKitta').fill(str(minimum_quantity_value))
-                                    page.locator('#crnNumber').fill(user['crn'])
-                                    page.locator('#disclaimer').click()
+                                        page.select_option('#selectBank', index=1)
+                                        page.select_option('#accountNumber', index=1)
+                                        page.locator('#appliedKitta').fill(str(minimum_quantity_value))
+                                        page.locator('#crnNumber').fill(user['crn'])
+                                        page.locator('#disclaimer').click()
 
-                                    # PIN confirmation
-                                    proceed_button = page.wait_for_selector('.card-footer button.btn-primary:visible')
-                                    if proceed_button:
-                                        proceed_button.click()
-                                        page.locator('#transactionPIN').fill(user['pin'])
-                                        page.wait_for_selector('.confirm-page-btn .btn-primary').click()
-                                        username = user['username']
-                                        print(f"Applied for {username}.")
+                                        # PIN confirmation
+                                        proceed_button = page.wait_for_selector('.card-footer button.btn-primary:visible')
+                                        if proceed_button:
+                                            proceed_button.click()
+                                            page.locator('#transactionPIN').fill(user['pin'])
+                                            page.wait_for_selector('.confirm-page-btn .btn-primary').click()
+                                            print(f"Applied for {name} ({username}).")
+                                        else:
+                                            print(f"Not applied for {name} ({username}).")
+                                    else:
+                                        print(f"Already applied for {name} ({username}).")
                             else:
                                 print(f"No new IPO or FPO found.")
+                        else:
+                            print(f"No new IPO or FPO found.")
 
                 except Exception as e:
                     print(f"Exception occurred: {str(e)}")
